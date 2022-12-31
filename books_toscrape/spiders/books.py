37de3573cebc4f8c386f4c070a757d15b4568997
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2022-12-31 15:22:19 +0330

    FEED settings added

--- books_toscrape/spiders/books.py
+++ books_toscrape/spiders/books.py
@@ -1,10 +1,18 @@
 import scrapy
-
+import datetime
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
+
+    custom_settings = {
+        'FEED_URI': f'output/books_{datetime.datetime.today().strftime("%Y-%M-%d")}.csv',
+        'FEED_FORMAT': 'csv',
+        'FEED_EXPORTERS': {'csv': 'scrapy.exporters.CsvItemExporter'},
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'FEED_EXPORT_FIELDS': ('title', 'price', 'rating') 
+    }
 
     def parse(self, response):
         for book in response.xpath("//article[@class='product_pod']"):
